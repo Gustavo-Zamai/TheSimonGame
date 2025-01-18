646e69c26fa36c79651ef385f36df0a4c3dff9fd
Gustavo Zamai <gustavosimaozamai@gmail.com>
2025-01-18 19:30:52 -0300

    Add some functions to start the game and also clicked square

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,29 @@
+function generateRandomNumber() {
+    return Math.floor((Math.random() * 4) + 1);
+}
 
+function pickASquare() {
+    var randomNumber = generateRandomNumber();
+    var selectedSquare = $("button." + randomNumber);
 
+    selectedSquare.addClass("active");
 
+    setTimeout(() => {
+        selectedSquare.removeClass("active");
+    }, 200);
+}
 
+$("body").on("keydown", function (e) {
+    console.log(e);
+    pickASquare();
+});
 
+var clickedSquare = $("button").on("click", function clicked(e) {
+    console.log(e)
+    clickedSquare.addClass("active");
 
+    setTimeout(() => {
+        clickedSquare.removeClass("active");
+    }, 200);    
 
-
-$("body").on("keydown", function(e){
-    console.log(e.key);
 })
